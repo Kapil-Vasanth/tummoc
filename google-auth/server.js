a73c0dc8e7c0ce7b49f5dfa1cc398205f0ca295d
Kapil <kapilvasanthbalearo@gmail.com>
2023-07-12 09:05:55 +0530

    google-auth update
    This is the updated version of google auth

--- google-auth/server.js
+++ google-auth/server.js
@@ -1,32 +1,32 @@
 require("dotenv").config();
-const express = require("express")
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const passport = require("passport");
-const cookieSession = require("cookie-session");
-const passportSetup = require("./passport");
 const authRoute = require("./routes/auth");
+const cookieSession = require("cookie-session");
+const passportStrategy = require("./passport");
 const app = express();
 
 app.use(
-    cookieSession({
-        name: "session",
-        keys: ['tummoc'],
-        maxAge: 24* 60 * 60 * 100,
-    })
+	cookieSession({
+		name: "session",
+		keys: ["cyberwolve"],
+		maxAge: 24 * 60 * 60 * 100,
+	})
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(
-    cors({
-        origin:"http://localhost:3000",
-        methods: "GET,POST,PUT,DELETE",
-        credentials: true,
-    })
-)
+	cors({
+		origin: "http://localhost:3000",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
 
-app.use("/auth",authRoute);
+app.use("/auth", authRoute);
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+app.listen(port, () => console.log(`Listenting on port ${port}...`));
