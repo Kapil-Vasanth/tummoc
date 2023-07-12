5370bdd044bf87e268cae6877565480d3f1cd596
Kapil <kapilvasanthbalearo@gmail.com>
2023-07-12 21:16:58 +0530

    Updating google auth file

--- google-auth/routes/auth.js
+++ google-auth/routes/auth.js
@@ -23,7 +23,7 @@ router.get("/login/failed", (req,res) => {
 router.get(
     "/google/callback",
     passport.authenticate("google",{
-        successRedirect: process.env.CLIENT_URL,
+        successRedirect: "/",
         failureRedirect: "/login/failed",
     })
 );
