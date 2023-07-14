c9abcd62f22dc4fcfe47900bb3c492d7f8c625af
Kapil-Vasanth <118164273+Kapil-Vasanth@users.noreply.github.com>
2023-07-14 12:28:37 +0530

    Update auth.js

--- google-auth/routes/auth.js
+++ google-auth/routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 
 router.get("/login/success", (req,res) => {
-    console.log("user is authorized "+req.user)
+    console.log("user is authorized")
     if(req.user){
         res.status(200).json({
             error: false,
